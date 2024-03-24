55251ac6924332173543a0ec9ab0a45d40c8d127
Samuel Teixeira de Abreu Filho <samuel.abreu@fleetcor.com.br>
2024-03-24 09:44:00 -0300

    feat(structure): create a struct of beers and create the other files

---
+++ core/beer/entity.go
@@ -0,0 +1,74 @@
+package beer
+
+type Beer struct {
+	ID    int64     `json:"id"`
+	Name  string    `json:"name"`
+	Type  BeerType  `json:"type"`
+	Style BeerStyle `json:"style"`
+}
+
+type BeerType int
+
+const (
+	TypeAle   = 1
+	TypeLager = 2
+	TypeMalt  = 3
+	TypeStout = 4
+)
+
+func (t BeerType) String() string {
+	switch t {
+	case TypeAle:
+		return "Ale"
+	case TypeLager:
+		return "lager"
+	case TypeMalt:
+		return "Malt"
+	case TypeStout:
+		return "Stout"
+	}
+	return "Unknow"
+}
+
+type BeerStyle int
+
+const (
+	StyleAmber = iota + 1
+	StyleBlonde
+	StyleBrown
+	StyleCream
+	StyleDark
+	StylePale
+	StyleStrong
+	StyleWheat
+	StyleRed
+	StyleIPA
+	StyleLime
+)
+
+func (t BeerStyle) String() string {
+	switch t {
+	case StyleAmber:
+		return "Amber"
+	case StyleBlonde:
+		return "Blonde"
+	case StyleBrown:
+		return "Brown"
+	case StyleCream:
+		return "Cream"
+	case StyleDark:
+		return "Dark"
+	case StylePale:
+		return "Pale"
+	case StyleStrong:
+		return "Strong"
+	case StyleWheat:
+		return "Wheat"
+	case StyleRed:
+		return "Red"
+	case StyleIPA:
+		return "IPA"
+	case StyleLime:
+		return "Lime"
+	}
+}
